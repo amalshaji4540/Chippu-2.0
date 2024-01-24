0643dd88a72fbd4778f66769171844a28ae3e02c
Amalshaji4540 <amalshaji4540@gmail.com>
2024-01-24 23:18:07 +0530

    Motor controller using angular velocity and linear velocity

--- Motor_controller/Motor_controller.ino
+++ Motor_controller/Motor_controller.ino
@@ -1,29 +1,71 @@
-#define Motor1Pin1 16
-#define Motor1Pin2 17
+#include <math.h>
+
+#define L_FORW 16
+#define L_BACK 17
+#define R_FORW 32
+#define R_BACK 33
+#define enable2Pin1 15
 #define enable1Pin1 5
+
+float mapPwm(float x, float out_min, float out_max);
+
+int PWM_MIN =181;
+int PWMRANGE =255;
+
 void setup() {
   // put your setup code here, to run once:
-    pinMode(Motor1Pin1,OUTPUT);
-    pinMode(Motor1Pin2,OUTPUT);
+    pinMode(L_FORW,OUTPUT);
+    pinMode(L_BACK,OUTPUT);
+    pinMode(R_FORW,OUTPUT);
+    pinMode(R_BACK,OUTPUT);
     pinMode(enable1Pin1,OUTPUT);
+    pinMode(enable2Pin1,OUTPUT);
 
     const int freq = 30000;
-    const int pwmChannel = 0;
+    const int pwmChannel1 = 0;
+    const int pwmChannel2 = 1;
     const int resolution = 8;
     int dutyCycle = 200;
 
-    digitalWrite(Motor1Pin1,HIGH);
-    digitalWrite(Motor1Pin2,LOW);
 
-    ledcSetup(pwmChannel, freq, resolution);
-    ledcAttachPin(enable1Pin1, pwmChannel);
-    ledcWrite(pwmChannel, 160);
+    ledcSetup(pwmChannel1, freq, resolution);
+    ledcAttachPin(enable1Pin1, pwmChannel1);
+
+    ledcSetup(pwmChannel2, freq, resolution);
+    ledcAttachPin(enable2Pin1, pwmChannel2);
+
+    Serial.begin(9600);
+    float x = max(min(1.0f, 1.0f), -1.0f);
+    float z = max(min(1.0f, 1.0f), -1.0f);
+
+    float l = (x - z) / 2;
+    float r = (x + z) / 2;
+
+    uint16_t lPwm = mapPwm(fabs(l), PWM_MIN, PWMRANGE);
+    uint16_t rPwm = mapPwm(fabs(r), PWM_MIN, PWMRANGE);
+
+    digitalWrite(L_FORW, l > 0);
+    digitalWrite(L_BACK, l < 0);
+    digitalWrite(R_FORW, r > 0);
+    digitalWrite(R_BACK, r < 0);
+    ledcWrite(pwmChannel1, lPwm);
+    ledcWrite(pwmChannel2, rPwm);
 
 
     
+    Serial.println(x);
+    Serial.println(z);
+
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+float mapPwm(float x, float out_min, float out_max)
+{
+  return x * (out_max - out_min) + out_min;
 }
