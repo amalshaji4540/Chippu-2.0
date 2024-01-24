2745db0e50b2a5cf1dc0e3c7b6a2dc162be06662
Amalshaji4540 <amalshaji4540@gmail.com>
2024-01-24 21:56:20 +0530

    1 motor controller with pwm

--- Motor_controller/Motor_controller.ino
+++ Motor_controller/Motor_controller.ino
@@ -1,9 +1,26 @@
 #define Motor1Pin1 16
 #define Motor1Pin2 17
-#define EnablePin1 5
+#define enable1Pin1 5
 void setup() {
   // put your setup code here, to run once:
-    pinMode(Motor1Pin1 16)
+    pinMode(Motor1Pin1,OUTPUT);
+    pinMode(Motor1Pin2,OUTPUT);
+    pinMode(enable1Pin1,OUTPUT);
+
+    const int freq = 30000;
+    const int pwmChannel = 0;
+    const int resolution = 8;
+    int dutyCycle = 200;
+
+    digitalWrite(Motor1Pin1,HIGH);
+    digitalWrite(Motor1Pin2,LOW);
+
+    ledcSetup(pwmChannel, freq, resolution);
+    ledcAttachPin(enable1Pin1, pwmChannel);
+    ledcWrite(pwmChannel, 160);
+
+
+    
 }
 
 void loop() {
