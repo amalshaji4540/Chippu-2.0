7a01e817a569c1584c0b1dbd5275ce5dcd81e8a9
Amalshaji4540 <amalshaji4540@gmail.com>
2024-01-24 21:20:15 +0530

    started the chippu project.Started working on motor controller

---
+++ Motor_controller/Motor_controller.ino
@@ -0,0 +1,12 @@
+#define Motor1Pin1 16
+#define Motor1Pin2 17
+#define EnablePin1 5
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(Motor1Pin1 16)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
