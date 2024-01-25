cd119b7df634ea03cec123c22c1d4abf40969a96
Amalshaji4540 <amalshaji4540@gmail.com>
2024-01-25 10:57:16 +0530

    added publisher or publishing encoded value

--- micro_Ros_motor_controller/micro_Ros_motor_controller.ino
+++ micro_Ros_motor_controller/micro_Ros_motor_controller.ino
@@ -5,6 +5,7 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <geometry_msgs/msg/twist.h>
+#include <std_msgs/msg/int32.h>
 
 rcl_subscription_t subscriber;
 geometry_msgs__msg__Twist msg;
@@ -12,6 +13,12 @@ rclc_executor_t executor;
 rcl_allocator_t allocator;
 rclc_support_t support;
 rcl_node_t node;
+
+rcl_publisher_t publisher1;
+rcl_publisher_t publisher2;
+std_msgs__msg__Int32 encodervalue_l;
+std_msgs__msg__Int32 encodervalue_r;
+rcl_timer_t timer;
 
 #define LED_PIN 2
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
@@ -24,6 +31,13 @@ rcl_node_t node;
 #define R_BACK 33
 #define enable2Pin1 15
 #define enable1Pin1 5
+
+int encoderPin1 = 16; //Encoder Output 'A' must connected with intreput pin of arduino.
+int encoderPin2 = 17; //Encoder Otput 'B' must connected with intreput pin of arduino.
+int encoderPin3 = 18;
+int encoderPin4 = 5;
+int lastEncoded_r =0;
+int lastEncoded_l =0;
 
 float mapPwm(float x, float out_min, float out_max);
 
@@ -42,6 +56,16 @@ void error_loop(){
   }
 }
 
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{  
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher1, &encodervalue_l, NULL));
+    RCSOFTCHECK(rcl_publish(&publisher2, &encodervalue_r, NULL));
+    
+  }
+}
+
 //twist message cb
 void subscription_callback(const void *msgin) {
   const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
@@ -55,8 +79,8 @@ void subscription_callback(const void *msgin) {
   float l = (x - z) / 2;
   float r = (x + z) / 2;
 
-  uint16_t lPwm = mapPwm(fabs(l), PWM_MIN, PWMRANGE);
-  uint16_t rPwm = mapPwm(fabs(r), PWM_MIN, PWMRANGE);
+  uint16_t lPwm = map(l,-1,1,PWM_MIN,PWMRANGE);
+  uint16_t rPwm = map(r,-1,1,PWM_MIN,PWMRANGE);
 
     digitalWrite(L_FORW, l > 0);
     digitalWrite(L_BACK, l < 0);
@@ -74,6 +98,24 @@ void setup() {
     pinMode(R_BACK,OUTPUT);
     pinMode(enable1Pin1,OUTPUT);
     pinMode(enable2Pin1,OUTPUT);
+
+    digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+    digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+
+    pinMode(encoderPin1, INPUT_PULLUP);
+    pinMode(encoderPin2, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(encoderPin1), updateEncoder_l, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(encoderPin2), updateEncoder_l, CHANGE);
+
+
+     digitalWrite(encoderPin3, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin4, HIGH); //turn pullup resistor on
+  pinMode(encoderPin3, INPUT_PULLUP); 
+  pinMode(encoderPin4, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoderPin3), updateEncoder_r, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderPin4), updateEncoder_r, CHANGE);
+
 
     ledcSetup(pwmChannel1, freq, resolution);
     ledcAttachPin(enable1Pin1, pwmChannel1);
@@ -103,9 +145,33 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
     "cmd_vel"));
 
+  // create publisher for left wheel
+  RCCHECK(rclc_publisher_init_default(
+    &publisher1,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "left_wheel_tick"));
+    //create a publisher for right wheel
+    RCCHECK(rclc_publisher_init_default(
+    &publisher2,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "right_wheel_tick"));
+
+  const unsigned int timer_timeout = 100;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
   // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  encodervalue_l.data = 0;
+  encodervalue_r.data = 0;
 
 }
 
@@ -114,7 +180,29 @@ void loop() {
   RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
 
-float mapPwm(float x, float out_min, float out_max)
-{
-  return x * (out_max - out_min) + out_min;
+void updateEncoder_l(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+  int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded_l << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encodervalue_l.data --;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encodervalue_l.data ++;
+
+  lastEncoded_l = encoded; //store this value for next time
+  
 }
+
+void updateEncoder_r(){
+  int MSB = digitalRead(encoderPin3); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin4); //LSB = least significant bit
+  int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded_r << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encodervalue_r.data --;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encodervalue_r.data ++;
+
+  lastEncoded_r = encoded; //store this value for next time
+  
+}
+
